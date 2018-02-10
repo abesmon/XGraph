cabfc508337ffeabd971b098862379adec804011
Alexey Lysenko <abesmon@gmail.com>
2018-02-10 22:54:07 +0300

    Initial Commit

---
+++ ConnectedGraph/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ConnectedGraph
+//
+//  Created by Алексей Лысенко on 10.02.18.
+//  Copyright © 2018 Alexey Lysenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
